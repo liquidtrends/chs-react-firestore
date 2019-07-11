daec4837dbcf71089d5a4e9a36b2b7a4ee2128a8
Richard <srjrconsulting@gmail.com>
2019-07-11 12:24:37 -0400

    add Charts, Table to PDF

--- src/components/ExportData.js
+++ src/components/ExportData.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import firebase from '../Firebase';
+import SortableTbl from 'react-sort-search-table';
+import StatusChart from './StatusChart';
+import AncestryChart from './AncestryChart';
 
 export default class Export extends Component {
   constructor(props) {
     super(props);
+    this.ref = firebase.firestore().collection('cases');
+    this.unsubscribe = null;
+    this.state = {
+      cases: [],
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const cases = [];
+    querySnapshot.forEach((doc) => {
+      const { firstName, lastName, caseStatus, phoneNumber, reasonForIntervention, referredBy } = doc.data();
+      cases.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        firstName,
+        lastName,
+        caseStatus,
+        phoneNumber,
+        reasonForIntervention,
+        referredBy
+      });
+    });
+    this.setState({ cases });
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
   }
 
   printDocument() {
@@ -14,27 +46,44 @@ export default class Export extends Component {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF();
         pdf.addImage(imgData, 'JPEG', 0, 0);
-        // pdf.output('dataurlnewwindow');
         pdf.save("download.pdf");
-      })
-    ;
+      });
   }
 
   render() {
-    return (<div>
+    const { cases } = this.state;
+    let col = ["key", "firstName", "lastName", "caseStatus"];
+    let tHead = ["CASE ID", "First Name", "Last Name", "Status"];
+
+    return (<React.Fragment>
       <div className="mb5">
-        <button onClick={this.printDocument}>Print</button>
+        <button onClick={this.printDocument}>PDF Download</button>
       </div>
-      <div id="divToPrint" className="mt4" style={{
-        backgroundColor: '#f5f5f5',
+      <Grid container id="divToPrint" className="mt4" style={{
         width: '210mm',
-        minHeight: '297mm',
+        minHeight: '397mm',
         marginLeft: 'auto',
-        marginRight: 'auto'
+        marginRight: 'auto',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: 'darkgrey'
       }}>
-        <div>Note: Here the dimensions of div are same as A4</div> 
-        <div>You Can add any component here</div>
-      </div>
-    </div>);
+        <Grid item xs={12}>
+          <SortableTbl
+            tblData={cases}
+            tHead={tHead}
+            dKey={col}
+            paging={false}
+            search={false}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item xs={6}><StatusChart/></Grid>
+            <Grid item xs={6}><AncestryChart/></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>);
   }
 }
