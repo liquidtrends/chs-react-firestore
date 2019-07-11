e6d0b536662043677f21e017fb17ed7fbc242e5a
Richard <srjrconsulting@gmail.com>
2019-07-10 22:13:17 -0400

    Ancestry Chart update

--- src/components/AncestryChart.js
+++ src/components/AncestryChart.js
@@ -5,16 +5,46 @@ import firebase from '../Firebase';
 import {Pie} from 'react-chartjs-2';
 // import { Doughnut } from 'react-chartjs-2';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import AncestryCount from './AncestryCount';
+// import AncestryCount from './AncestryCount';
 
+const BG_COLORS = ["#3498db", "#9b59b6", "#e74c3c", "#34495e"];
+const HOVER_BG_COLORS = ["#2980b9", "#8e44ad", "#c0392b", "#2c3e50"];
 class AncestryChart extends Component {
 
   constructor(props) {
     super(props);
+    this.ancestryRef = firebase.firestore().collection('ancestry');
+    this.unsubscribe = null;
+    this.state = {
+      ancestry: [],
+      bgColors: BG_COLORS,
+      hoverBgColors: HOVER_BG_COLORS
+    };
   }
 
+  onCollectionUpdate = (querySnapshot) => {
+    const ancestry = [];
+    querySnapshot.forEach((doc) => {
+      const { metis, inuit, status, nonStatus } = doc.data();
+      ancestry.push([
+        status,
+        nonStatus,
+        metis,
+        inuit
+      ]);
+    });
+    this.setState({
+      ancestry
+    });
+  };
+
+  componentDidMount() {
+    this.unsubscribe = this.ancestryRef.onSnapshot(this.onCollectionUpdate);
+  };
+
   render() {
-    const ancestryData = {
+    const { ancestry, bgColors, hoverBgColors } = this.state;
+    const ancestryChartData = {
       labels: [
         "Status",
         "Non-Status",
@@ -22,35 +52,22 @@ class AncestryChart extends Component {
         "Inuit"
       ],
       datasets: [{
-        data: <AncestryCount />,
-        backgroundColor: [
-          "#3498db",
-          "#9b59b6",
-          "#e74c3c",
-          "#34495e"
-        ],
-        hoverBackgroundColor: [
-          "#2980b9",
-          "#8e44ad",
-          "#c0392b",
-          "#2c3e50"
-        ]
+        data: ancestry[0],
+        backgroundColor: bgColors,
+        hoverBackgroundColor: hoverBgColors
       }]
     };
 
     return (
-
-      <div>
+      <React.Fragment>
         <Pie
           width={100}
           height={75}
-          data={ancestryData}
+          data={ancestryChartData}
         />
-        <AncestryCount />
-      </div>
-
+        {/* <AncestryCount /> */}
+      </React.Fragment>
     )
   }
-
 }
 export default AncestryChart;
