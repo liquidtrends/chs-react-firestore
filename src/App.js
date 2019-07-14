7211759636b01af49f9942d1f118cd1223b25cd7
Richard <srjrconsulting@gmail.com>
2019-07-14 07:28:05 -0400

    App, index configuration change

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
-import firebase from './Firebase';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-import IconButton from '@material-ui/core/IconButton';
-import { makeStyles } from '@material-ui/core/styles';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-import AncestryChart from './components/AncestryChart';
-import StatusChart from './components/StatusChart';
-import DataTable from './components/DataTable';
+import './App.css';
+import CaseFiles from './components/CaseFiles';
+import Create from './components/Create';
+import Dashboard from  './components/Dashboard';
+import Edit from './components/Edit';
+import Export from './components/ExportData';
+import Show from './components/Show';
+import Navigation from './components/Navigation';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
+import { withAuthentication } from './components/Session';
 
-
-class App extends Component {
-
-  render() {
-    return (
-      <div className="container">
-        <div className="panel panel-default">
-          <div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="card">
-                  <h4>Casefile Status</h4>
-                  <StatusChart />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="card">
-                  <h4>Ancestry</h4>
-                  <AncestryChart />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <DataTable />
-          </div>
-
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="top-bar"></div>
+    <div className="menu-container">
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="/Casefiles">Case Files</Nav.Link>
+              <Nav.Link href="/export">Export Data</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+              <Nav.Item>
+                <Nav.Link href="/Create"><Button variant="contained" className="new-casefile-button">New Case File</Button></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </div>
+    <div>
+      <Route exact path='/' component={() => {
+        window.location.href = '/signin';
+        return null;
+      }}/>
+      <Route exact path='/dashboard' component={Dashboard} />
+      <Route path='/edit/:id' component={Edit} />
+      <Route path='/create' component={Create} />
+      <Route path='/show/:id' component={Show} />
+      <Route path='/casefiles/' component={CaseFiles} />
+      <Route path='/export' component={Export} />
+    </div>
+  </Router>
+);
 
 export default App;
+// export default withAuthentication(App);
--- src/index.js
+++ src/index.js
@@ -1,56 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Button from '@material-ui/core/Button';
-import CaseFiles from './components/CaseFiles';
-import Create from './components/Create';
-import Dashboard from  './components/Dashboard';
-import Edit from './components/Edit';
-import Export from './components/ExportData';
-import Show from './components/Show';
-import Navigation from './components/Navigation';
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import App from './App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <Router>
-    <div className="top-bar"></div>
-    <div className="menu-container">
-      <Container>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-              <Nav.Link href="/Casefiles">Case Files</Nav.Link>
-              <Nav.Link href="/export">Export Data</Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end">
-              <Nav.Item>
-                <Nav.Link href="/Create"><Button variant="contained" className="new-casefile-button">New Case File</Button></Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
-    </div>
-    <div>
-      <Route exact path='/' component={() => {
-        window.location.href = '/signin';
-        return null;
-      }}/>
-      <Route exact path='/dashboard' component={Dashboard} />
-      <Route path='/edit/:id' component={Edit} />
-      <Route path='/create' component={Create} />
-      <Route path='/show/:id' component={Show} />
-      <Route path='/casefiles/' component={CaseFiles} />
-      <Route path='/export' component={Export} />
-    </div>
-  </Router>,
+  <App />,
   document.getElementById('root')
 );
 
